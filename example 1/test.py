4c26a9d9208b603be1afa95e2e0142e41ba84427
jesus.canedo228508 <jesus.canedo228508@potros.itson.edu.mx>
2024-08-22 06:26:04 +0000

    prueba

--- example 1/test.py
+++ example 1/test.py
@@ -1,2 +1,11 @@
-print("hola")
-print("adios")
+#print("hola");
+#print("adios");
+
+#numero = int(input("ingresa un numero"));
+
+#if numero > 5:
+#    print("cinco es mayor que ", + numero);
+#else:
+#   print("cinco no es mayor que ", + numero);
+
+
