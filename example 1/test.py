c454ea4c676e6fa34da0f9234454864d2548c7eb
jesus.canedo228508 <jesus.canedo228508@potros.itson.edu.mx>
2024-09-02 01:36:17 +0000

    list

--- example 1/test.py
+++ example 1/test.py
@@ -8,7 +8,29 @@
 # else:
 #     print("El número no es mayor que 5:", numero)
 
-i = 0
-for i in range(5):
-    print(i)
+# i = 0
+# for i in range(5):
+#     print(i)
+
+numeros = [5, 4.23, 3.56, 2, 1]
+
+print("contenido: ", numeros)
+print("tamaño: ", len(numeros))
+del numeros[3]
+print("primer elemento: ", numeros[0])
+numeros[-1] = numeros[2]
+print("ultimo elemento: ", numeros[-1])
+
+formato = []
+
+for x in numeros:
+    formato.append(x)
+
+print(formato)
+
+total = 0
+for i in numeros:
+    total += i
+
+print("total: ", total)
 
